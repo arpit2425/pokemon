db88a71527681254e841b2d146cb363e26ddd94f
Arpit Trivedi <arpittrivedi2425@gmail.com>
2020-05-21 23:33:00 +0530

    Styling 3

---
+++ src/PokeCard.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "./PokeCard.css";
+const baseUrl = " https://assets.pokemon.com/assets/cms2/img/pokedex/detail/";
+class PokeCard extends Component {
+  render() {
+    const url = `${baseUrl}${this.props.id}.png`;
+    // console.log(url)
+    return (
+      <div className="Pokecard">
+        <h1>{this.props.name}</h1>
+        <img src={url} alt={this.props.name} />
+        <h4 className="pokecard-data">{this.props.type}</h4>
+        <h4 className="pokecard-data">{this.props.exp}</h4>
+      </div>
+    );
+  }
+}
+
+export default PokeCard;
---
+++ src/Pokedex.js
@@ -0,0 +1,39 @@
+import React, { Component } from "react";
+import PokeCard from "./PokeCard";
+import "./Pokedex.css";
+
+class Pokedex extends Component {
+  static defaultProps = {
+    pokemon: [
+      { id: "004", name: "Charmander", type: "fire", base_experience: 62 },
+      { id: "007", name: "Squirtle", type: "water", base_experience: 63 },
+      { id: "011", name: "Metapod", type: "bug", base_experience: 72 },
+      { id: "012", name: "Butterfree", type: "flying", base_experience: 178 },
+      { id: "025", name: "Pikachu", type: "electric", base_experience: 112 },
+      { id: "039", name: "Jigglypuff", type: "normal", base_experience: 95 },
+      { id: "094", name: "Gengar", type: "poison", base_experience: 225 },
+      { id: "133", name: "Eevee", type: "normal", base_experience: 65 },
+    ],
+  };
+  render() {
+    console.log(this.props);
+    return (
+      <div>
+        <h1>Pokedex</h1>
+        <div className="Pokedex-card">
+          {this.props.pokemon.map((p) => (
+            <PokeCard
+              key={p.id}
+              id={p.id}
+              name={p.name}
+              type={p.type}
+              exp={p.base_experience}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Pokedex;
